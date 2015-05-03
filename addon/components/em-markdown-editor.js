f4995ad7db191dce5e2b07bdeee2f9dd0aec9314
Sang Cu <sangcn@gmail.com>
2015-05-03 15:49:42 +0700

    call update layout when initialize. Also, adjust the  default layout with 50% between editor and preview

--- addon/components/em-markdown-editor.js
+++ addon/components/em-markdown-editor.js
@@ -2,16 +2,24 @@ import Ember from 'ember';
 import layout from '../templates/components/em-markdown-editor';
 
 export default Ember.Component.extend({
-	content: "",
+    content: "",
     layout: layout,
 
-	lang: {
+    lang: {
         preview: "Preview",
         editor: "Editor",
         fullscreen: "Click to toggle fullscreen"
     },
 
+    onInit: Ember.on('didInsertElement', function() {
+        this.updateLayout();
+    }),
+
     onTyping: Ember.observer('content', function() {
+        this.updateLayout();
+    }),
+
+    updateLayout: function() {
         var lines = this.get("content").split('\n');
         var height = lines.length * 15;
         var previewHeight = this.$('.em-markdown-preview').css('height');
@@ -20,26 +28,32 @@ export default Ember.Component.extend({
             height = previewHeight;
         }
 
-        this.$('textarea, .em-markdown-preview').css('height', height);
+        this.$('textarea, .em-markdown-output').css('height', height);
 
-    }),
+    },
+
     doubleClick: function() {
         this.$().children().toggleClass("fullscreen");
     },
+
     drop: function(ev) {
-    	ev.preventDefault();
-    	this.sendAction("onUpload",ev);
+        ev.preventDefault();
+        this.sendAction("onUpload", ev);
         Ember.debug("you dropped something");
     },
+
     dragEnter: function() {
-    	this.$('.em-markdown-editor').addClass("drop-file-enter");
+        this.$('.em-markdown-editor').addClass("drop-file-enter");
     },
+
     dragLeave: function() {
-		this.$('.em-markdown-editor').removeClass("drop-file-enter");
+        this.$('.em-markdown-editor').removeClass("drop-file-enter");
     },
-    dragOver:function(){
-this.$('.em-markdown-editor').addClass("drop-file-enter");
+
+    dragOver: function() {
+        this.$('.em-markdown-editor').addClass("drop-file-enter");
     },
+
     actions: {
         toggleFullscreen: function() {
             this.doubleClick();
